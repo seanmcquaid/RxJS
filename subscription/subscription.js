f75689c788d9ff69cb16a69edb42faf6f0f9a9ef
seanmcquaid <sean.mcquaid2@verizon.net>
2021-01-05 22:24:08 -0500

    learned about subscriptions/unsubscribing

---
+++ subscription/subscription.js
@@ -0,0 +1,31 @@
+// What is a Subscription?
+// A Subscription is an object that represents a disposable resource, usually the execution of an Observable.
+// A Subscription has one important method, unsubscribe, that takes no argument and just disposes the resource held by the subscription.
+// In previous versions of RxJS, Subscription was called "Disposable".
+
+const { interval } = require('rxjs');
+
+const observable = interval(1000);
+const subscription = observable.subscribe((x) => console.log(x));
+// Later:
+// This cancels the ongoing Observable execution which
+// was started by calling subscribe with an Observer.
+subscription.unsubscribe();
+
+// Subscriptions can also be put together, so that a call to an unsubscribe() of one Subscription may unsubscribe multiple Subscriptions.
+// You can do this by "adding" one subscription into another:
+
+const observable1 = interval(400);
+const observable2 = interval(300);
+
+const subscription = observable1.subscribe((x) => console.log('first: ' + x));
+const childSubscription = observable2.subscribe((x) =>
+  console.log('second: ' + x)
+);
+
+subscription.add(childSubscription);
+
+setTimeout(() => {
+  // Unsubscribes BOTH subscription and childSubscription
+  subscription.unsubscribe();
+}, 1000);
