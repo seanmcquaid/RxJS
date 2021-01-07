06b1c6b50c88cd4c495cb01d73d33a4f5252ac0e
seanmcquaid <sean.mcquaid2@verizon.net>
2021-01-07 16:57:47 -0500

    finished learning about replay subjects

---
+++ subject/replaySubject.js
@@ -0,0 +1,67 @@
+// ReplaySubject
+// A ReplaySubject is similar to a BehaviorSubject in that it can send old values to new subscribers,
+// but it can also record a part of the Observable execution.
+
+// A ReplaySubject records multiple values from the Observable execution and replays them to new subscribers.
+
+// When creating a ReplaySubject, you can specify how many values to replay:
+
+const { ReplaySubject } = require('rxjs');
+const subject = new ReplaySubject(3); // buffer 3 values for new subscribers
+
+subject.subscribe({
+  next: (v) => console.log(`observerA: ${v}`),
+});
+
+subject.next(1);
+subject.next(2);
+subject.next(3);
+subject.next(4);
+
+subject.subscribe({
+  next: (v) => console.log(`observerB: ${v}`),
+});
+
+subject.next(5);
+
+// Logs:
+// observerA: 1
+// observerA: 2
+// observerA: 3
+// observerA: 4
+// observerB: 2
+// observerB: 3
+// observerB: 4
+// observerA: 5
+// observerB: 5
+
+// You can also specify a window time in milliseconds, besides of the buffer size, to determine how old the recorded values can be.
+// In the following example we use a large buffer size of 100, but a window time parameter of just 500 milliseconds.
+
+const subject = new ReplaySubject(100, 500 /* windowTime */);
+
+subject.subscribe({
+  next: (v) => console.log(`observerA: ${v}`),
+});
+
+let i = 1;
+setInterval(() => subject.next(i++), 200);
+
+setTimeout(() => {
+  subject.subscribe({
+    next: (v) => console.log(`observerB: ${v}`),
+  });
+}, 1000);
+
+// Logs
+// observerA: 1
+// observerA: 2
+// observerA: 3
+// observerA: 4
+// observerA: 5
+// observerB: 3
+// observerB: 4
+// observerB: 5
+// observerA: 6
+// observerB: 6
+// ...
